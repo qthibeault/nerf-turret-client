5c947e5a148dffba9fecca4585e9d7d2e5989603
Quinn Thibeault <quinn.thibeault96@gmail.com>
2023-05-02 10:50:40 -0700

    Create more descriptive errors for msgs module

--- src/turret_client/msgs.py
+++ src/turret_client/msgs.py
@@ -8,6 +8,10 @@ import msgpack
 from typing_extensions import Self
 
 
+class MsgError(Exception):
+    pass
+
+
 class Msg(ABC):
     @abstractmethod
     def to_dict(self) -> dict[str, object]:
@@ -18,14 +22,47 @@ class Msg(ABC):
         ...
 
 
+class MsgTypeError(MsgError):
+    pass
+
+
 def _check_msg_type(msg: dict[str, object], expected: str):
     try:
         msg_type = msg["type"]
     except KeyError:
-        raise ValueError("No message type provided")
+        raise MsgTypeError("No message type provided")
 
     if msg_type != expected:
-        raise ValueError(f"Wrong message type {msg_type}")
+        raise MsgTypeError(f"Expected message type [{expected}], got [{msg_type}]")
+
+
+T = TypeVar("T", int, float, str)
+
+
+class MsgDataError(MsgError):
+    pass
+
+
+def _get_msg_data(msg: dict[str, object], name: str, data_type: Type[T]) -> T:
+    try:
+        msg_data = msg[name]
+    except KeyError:
+        raise MsgDataError(f"Missing [{name}] in msg")
+
+    if not isinstance(msg_data, data_type):
+        raise MsgDataError(
+            f"[{name}] must be type [{data_type}], got [{type(msg_data)}]"
+        )
+
+    return msg_data
+
+
+def _check_int(value: object, name: str, lb: int | None = None) -> None:
+    if not isinstance(value, int):
+        raise TypeError(f"{name} must be an integer")
+
+    if lb is not None and value < lb:
+        raise ValueError(f"{name} must be greater than {lb}")
 
 
 @dataclass()
@@ -43,18 +80,8 @@ class MoveMsg(Msg):
     @classmethod
     def from_dict(cls, d: dict[str, object]) -> Self:
         _check_msg_type(d, "move")
-
-        try:
-            base_angle = d["base_angle"]
-            elev_angle = d["elev_angle"]
-        except KeyError:
-            raise ValueError("Missing base or elevation angle")
-
-        if not isinstance(base_angle, float):
-            raise TypeError(f"base angle must be float, not {type(base_angle)}")
-
-        if not isinstance(elev_angle, float):
-            raise TypeError(f"elevation angle must be float, not {type(elev_angle)}")
+        base_angle = _get_msg_data(d, "base_angle", float)
+        elev_angle = _get_msg_data(d, "elev_angle", float)
 
         return cls(base_angle, elev_angle)
 
@@ -62,6 +89,9 @@ class MoveMsg(Msg):
 @dataclass()
 class ShootMsg(Msg):
     times: int
+
+    def __post_init__(self):
+        _check_int(self.times, "times", 0)
 
     def to_dict(self) -> dict[str, object]:
         return {
@@ -72,14 +102,7 @@ class ShootMsg(Msg):
     @classmethod
     def from_dict(cls, d: dict[str, object]) -> Self:
         _check_msg_type(d, "shoot")
-
-        try:
-            times = d["times"]
-        except KeyError:
-            raise ValueError("Missing number of times to shoot")
-
-        if not isinstance(times, int):
-            raise TypeError("times must be an integer")
+        times = _get_msg_data(d, "times", int)
 
         return cls(times)
 
@@ -108,20 +131,16 @@ class DiscoverMsg(Msg):
 class AddrMsg(Msg):
     port: int
 
+    def __post_init__(self):
+        _check_int(self.port, "port", 1)
+
     def to_dict(self) -> dict[str, object]:
         return {"type": "address", "port": self.port}
 
     @classmethod
     def from_dict(cls, d: dict[str, object]) -> Self:
         _check_msg_type(d, "address")
-
-        try:
-            port = d["port"]
-        except KeyError:
-            raise ValueError("No port provided")
-
-        if not isinstance(port, int):
-            raise TypeError("Port must be an integer")
+        port = _get_msg_data(d, "port", int)
 
         return cls(port)
 
@@ -142,6 +161,21 @@ class StatusMsg(Msg):
     elev_angle: float
     shots: int
 
+    def __post_init__(self):
+        if isinstance(self.base_angle, int):
+            self.base_angle = float(self.base_angle)
+
+        if not isinstance(self.base_angle, float):
+            raise TypeError("base angle must be a float")
+
+        if isinstance(self.elev_angle, int):
+            self.elev_angle = float(self.elev_angle)
+
+        if not isinstance(self.elev_angle, float):
+            raise TypeError("elev angle must be a float")
+
+        _check_int(self.shots, "shots", 0)
+
     def to_dict(self) -> dict[str, object]:
         return {
             "type": "status",
@@ -153,21 +187,9 @@ class StatusMsg(Msg):
     @classmethod
     def from_dict(cls, d: dict[str, object]) -> Self:
         _check_msg_type(d, "status")
-
-        try:
-            base_angle = d["base_angle"]
-            elev_angle = d["elevation_angle"]
-            shots = d["shots"]
-        except KeyError:
-            raise ValueError(
-                "base_angle, elevation_angle, or shots property missing from status message"
-            )
-
-        if not isinstance(base_angle, float) or not isinstance(elev_angle, float):
-            raise TypeError("Angles must be given as floats")
-
-        if not isinstance(shots, int):
-            raise TypeError("Shot counter must be an int")
+        base_angle = _get_msg_data(d, "base_angle", float)
+        elev_angle = _get_msg_data(d, "elevation_angle", float)
+        shots = _get_msg_data(d, "shots", int)
 
         return cls(base_angle, elev_angle, shots)
 
@@ -183,11 +205,23 @@ class ResetMsg(Msg):
 
 
 def encode(msg: Msg) -> bytes:
+    if not isinstance(msg, Msg):
+        raise TypeError(f"Can only encode values of type Msg, not [{type(msg)}]")
+
     return msgpack.packb(msg.to_dict())
 
 
 M = TypeVar("M", bound=Msg)
 
 
+class DecodeError(MsgError):
+    pass
+
+
 def decode(bs: bytes, msg_type: Type[M]) -> M:
+    msg_data = msgpack.unpackb(bs)
+
+    if not isinstance(msg_data, dict):
+        raise DecodeError("message did not unpack into dictionary")
+
     return msg_type.from_dict(msgpack.unpackb(bs))
